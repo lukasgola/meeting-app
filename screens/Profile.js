501031e7ac71374fbfe46feafaa49716b16b65d5
Łukasz Gola <lukaszgolaofficial@gmail.com>
2022-09-15 00:25:19 +0200

    Update Profile.js

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,5 +1,5 @@
 import React, {} from 'react';
-import {View, Dimensions, ScrollView, TouchableOpacity} from 'react-native';
+import {View, Dimensions, ScrollView, TouchableOpacity, ImageBackground, FlatList} from 'react-native';
 import {useTheme} from '../theme/ThemeProvider';
 
 import CustomText from '../components/CustomText';
@@ -22,77 +22,139 @@ export default function Profile(){
 
     const user = route.params.user;
 
-    const h2 = 16;
+    const data = [
+        {
+            id: 1
+        },
+        {
+            id: 2
+        },
+        {
+            id: 3
+        }
+    ]
 
 
     return (
         <ScrollView style={{ flex: 1, backgroundColor: colors.grey_l }}>
-            <TouchableOpacity 
-                onPress={() => navigation.navigate('EditProfile')}
-                style={{
-                    width: 30,
-                    height: 30,
-                    backgroundColor: colors.background,
-                    position: 'absolute',
-                    top: 10,
-                    right: 10,
-                    borderRadius: 15,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    shadowColor: "#000",
-                        shadowOffset: {
-                            width: 0,
-                            height: 10,
-                        },
-                        shadowOpacity: 0.40,
-                        shadowRadius: 4.84,
-                
-                        elevation: 6,
-            }}>
-                <Ionicons name='color-wand-outline' size={16} color={colors.text} />
-            </TouchableOpacity>
+            <ImageBackground 
+                source={{uri: user.avatar}} 
+                resizeMode="cover" 
+                blurRadius={2}
+                opac
+                style={{width: width, height: 200}}>
+                    <View style={{flex: 1, backgroundColor: 'rgba(0,0,0,0.25)'}}></View>
+            </ImageBackground>
             <View style={{
                 width: width,
                 borderTopLeftRadius: 30,
                 borderTopRightRadius: 30,
-                marginTop: -100,
                 backgroundColor: colors.background,
-                marginTop: 100,
+                marginTop: -100,
                 alignItems: 'center'
             }}>
-                <View style={{
-                    width: 104,
-                    height: 104,
-                    borderRadius: 52,
-                    marginTop: -50,
-                    backgroundColor: colors.background,
-                    justifyContent: 'center',
-                    alignItems: 'center',
+                <TouchableOpacity 
+                    onPress={() => navigation.navigate('EditProfile')}
+                    style={{
+                        width: 104,
+                        height: 104,
+                        borderRadius: 52,
+                        marginTop: -50,
+                        backgroundColor: colors.background,
+                        justifyContent: 'center',
+                        alignItems: 'center',
 
 
-                    shadowColor: "#000",
-                    shadowOffset: {
-                        width: 0,
-                        height: 10,
-                    },
-                    shadowOpacity: 0.40,
-                    shadowRadius: 4.84,
-            
-                    elevation: 6,
+                        shadowColor: "#000",
+                        shadowOffset: {
+                            width: 0,
+                            height: 6,
+                        },
+                        shadowOpacity: 0.15,
+                        shadowRadius: 5.84,
+
+                        elevation: 8,
                     
                 }}>
                     <UserIcon size={100} photo={user.avatar} score={user.score} />
-                </View>
+                </TouchableOpacity>
                 <View style={{alignItems: 'center', height: 80, justifyContent: 'space-around', paddingVertical: 5, marginTop: 10}}>
                     <CustomText weight='bold' size={20} >{user.username}</CustomText>
                     <CustomText >{user.email}</CustomText>
                 </View>
-                <View style={{width: width, alignItems: 'center', marginTop: 20}}>
-                    <TouchableOpacity style={{width: 100, height: 40, borderRadius: 20, backgroundColor: colors.primary, justifyContent: 'center', alignItems: 'center'}}>
+                <View style={{width: width, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-around', marginTop: 20}}>
+                    <TouchableOpacity 
+                            style={{
+                                width: 80, 
+                                height: 40, 
+                                borderRadius: 20, 
+                                backgroundColor: colors.background, 
+                                justifyContent: 'center', 
+                                alignItems: 'center',
+                                shadowColor: '#000',
+                                shadowOffset: {
+                                    width: 0,
+                                    height: 6,
+                                },
+                                shadowOpacity: 0.15,
+                                shadowRadius: 5.84,
+
+                                elevation: 8,
+                            }}>
+                            <Ionicons name='chatbubbles-outline' size={25} color={colors.primary} />
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        style={{
+                            width: 100, 
+                            height: 40, 
+                            borderRadius: 20, 
+                            backgroundColor: colors.primary, 
+                            justifyContent: 'center', 
+                            alignItems: 'center',
+                            shadowColor: colors.primary,
+                            shadowOffset: {
+                                width: 0,
+                                height: 6,
+                            },
+                            shadowOpacity: 0.4,
+                            shadowRadius: 5.84,
+
+                            elevation: 8,
+                        }}>
                         <CustomText weight='bold' color={colors.background}>FOLLOW</CustomText>
                     </TouchableOpacity>
+                    <TouchableOpacity 
+                        style={{
+                            width: 80, 
+                            height: 40, 
+                            borderRadius: 20, 
+                            backgroundColor: colors.background, 
+                            justifyContent: 'center', 
+                            alignItems: 'center',
+                            shadowColor: '#000',
+                            shadowOffset: {
+                                width: 0,
+                                height: 6,
+                            },
+                            shadowOpacity: 0.15,
+                            shadowRadius: 5.84,
+
+                            elevation: 8,
+                        }}>
+                        <Ionicons name='share-outline' size={25} color={colors.primary} />
+                    </TouchableOpacity>
                 </View>
-                <View style={{width: width, height: 100, flexDirection: 'row', marginTop: 10, justifyContent: 'space-around'}}>
+                <View 
+                    style={{
+                        width: width, 
+                        height: 90, 
+                        flexDirection: 'row', 
+                        marginTop: 25, 
+                        justifyContent: 'space-around', 
+                        borderColor: colors.grey_l, 
+                        borderTopWidth: 1,
+                        borderBottomWidth: 1
+                    }}>
                     <View style={{width: width*0.3, height: '100%', justifyContent: 'space-around', alignItems: 'center', paddingVertical: 20}}>
                         <CustomText weight='bold' size={20}>{user.score}</CustomText>
                         <CustomText color={colors.grey_d}>Score</CustomText>
@@ -106,10 +168,50 @@ export default function Profile(){
                         <CustomText color={colors.grey_d}>Following</CustomText>
                     </View>
                 </View>
+
+                <View style={{width: width}}>
+                    <View
+                        style={{marginLeft: 20, marginTop: 20}}>
+                        <CustomText weight='bold' size={16}>Last events</CustomText>
+                    </View>
+                    <View
+                        style={{
+                            width: '100%',
+                            marginTop: 10
+                        }}
+                    >
+                        <FlatList
+                            data={data}
+                            renderItem={({item}) => (
+                                <View
+                                    style={{
+                                        width: 60,
+                                        height: 60,
+                                        marginLeft: 20,
+                                        backgroundColor: colors.grey_l,
+                                        borderRadius: 20,
+                                        alignItems: 'center',
+                                        justifyContent: 'center'
+                                    }}
+                                >
+                                    <Ionicons name='bonfire-outline' size={25} color={colors.primary} />
+                                </View>
+                            )
+                            }
+                            keyExtractor={(item) => item.id}
+                            horizontal={true}
+                            showsHorizontalScrollIndicator={false}
+                            style={{
+                                marginTop: 10
+                            }}
+                        />
+                    </View>
+                </View>
+                
                 <View style={{width: width, alignItems: 'center', padding: 20}}>
                     <View
                         style={{width: '100%'}}>
-                        <CustomText weight='bold' size={h2}>About me</CustomText>
+                        <CustomText weight='bold' size={16}>About me</CustomText>
                     </View>
                     <View
                         style={{
@@ -118,6 +220,41 @@ export default function Profile(){
                         }}
                     >
                         <CustomText color={colors.grey_d}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to</CustomText>
+                    </View>
+                </View>
+
+                <View style={{width: width}}>
+                    <View
+                        style={{marginLeft: 20}}>
+                        <CustomText weight='bold' size={16}>Followers</CustomText>
+                    </View>
+                    <View
+                        style={{
+                            width: '100%',
+                            marginTop: 10,
+                            marginBottom: 50
+                        }}
+                    >
+                        <FlatList
+                            data={data}
+                            renderItem={({item}) => (
+                                <View
+                                    style={{
+                                        width: 60,
+                                        marginLeft: 20,
+                                    }}
+                                >
+                                    <UserIcon size={60} photo={user.avatar} score={user.score} />
+                                </View>
+                            )
+                            }
+                            keyExtractor={(item) => item.id}
+                            horizontal={true}
+                            showsHorizontalScrollIndicator={false}
+                            style={{
+                                marginTop: 10
+                            }}
+                        />
                     </View>
                 </View>
                 
