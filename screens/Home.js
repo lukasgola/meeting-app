2c28e02c04b8fe47058260a0f8695ac5c96d4694
Łukasz Gola <105663541+lukasgola@users.noreply.github.com>
2023-03-10 10:40:14 +0100

    optimazing

--- screens/Home.js
+++ screens/Home.js
@@ -125,106 +125,25 @@ export default function Main(){
                 keyExtractor={(item) => item.id}
                 showsVerticalScrollIndicator={false}
                 style={{width: width}}
-                ListHeaderComponent={
-                    <View style={{ width: width, marginVertical: 20}}>
-                        <View style={{width: width*0.9, marginLeft: width*0.05 }}>
-                            <View style={{ width: 0.9*width, flexDirection:'row' }}>
-                                <CustomText weight='bold' size={h1}>Welcome</CustomText>
-                                <CustomText weight='bold' size={h1} color={colors.primary}> {currentUser.username}</CustomText>
-                            </View>
-                            <CustomText size={h4} color={colors.text}>Check out the map</CustomText>
-                                
-                            <MapView 
-                                style={{
-                                    width: 0.9*width,
-                                    height: 120,
-                                    borderRadius: 10,
-                                    marginTop: 10,
-                                    marginBottom: 20,
-                                }}
-                                initialRegion={{
-                                    latitude: 50.5107,
-                                    longitude: 18.30056,
-                                    latitudeDelta: 0.0922,
-                                    longitudeDelta: 0.0421,
-                                }}
-                                provider={PROVIDER_GOOGLE}
-                                onPress={() => navigation.navigate('Map', { location: currentLocation, isEvent})}
-                                zoomEnabled={false}
-                                rotateEnabled={false}
-                                scrollEnabled={false}
-                            />
-
-                            
-                        </View>
-                        
-
-                        <View style={{width: width*0.9, marginLeft: width*0.05 }}>
-                            <View style={{ width: 0.9*width, flexDirection:'row', justifyContent: 'space-between' }}>
-                                <CustomText weight='bold' size={h2}>Popular Near You</CustomText>
-                                <TouchableOpacity
-                                >
-                                    <CustomText size={h4} color={colors.primary}>Show All</CustomText>
-                                </TouchableOpacity>
-                                
-                            </View>
-                            
-                            <CustomText size={h4} color={colors.grey_d}>Best parties</CustomText>
-                        </View>
-
-                        {parties ? 
-                        
-                            <View style={{ marginTop: 10 }}>
-                                {renderHorizontal(parties, renderPopularItem)}
-                            </View>
-
-                            : 
-                            
-                            <View style={{width: '100%', height: 170, justifyContent: 'center', alignItems: 'center'}}>
-                                <ActivityIndicator />
-                            </View>
-                        }
-                        
-                        
-                        <View style={{width: width*0.9, marginLeft: width*0.05 }}>
-                            <View style={{ width: 0.9*width,flexDirection:'row',justifyContent: 'space-between' }}>
-                                <CustomText weight='bold' size={h2}>Trending Users</CustomText>
-                                <TouchableOpacity
-                                >
-                                    <CustomText size={h4} color={colors.primary}>Show All</CustomText>
-                                </TouchableOpacity>
-                                
-                            </View>
-                            <CustomText size={h4} color={colors.grey_d}>Last week</CustomText>
-                        </View>
-
-                        {users ? 
-                        
-                            <View style={{marginTop: 10}}>
-                                {renderHorizontal(users, renderUser)}
-                            </View>
-
-                            : 
-                            
-                            <View style={{width: '100%', height: 170, justifyContent: 'center', alignItems: 'center'}}>
-                                <ActivityIndicator />
-                            </View>
-                        }
-                        
-                        <View style={{width: width*0.9, marginLeft: width*0.05 }}>
-                            <View style={{ width: 0.9*width, flexDirection:'row', justifyContent: 'space-between' }}>
-                                <CustomText weight='bold' size={20}>Parties</CustomText>
-                                <TouchableOpacity>
-                                    <CustomText size={h4} color={colors.primary}>Show All</CustomText>
-                                </TouchableOpacity>
-                            </View>
-                            <CustomText size={h4} color={colors.grey_d}>110 results found</CustomText>
-                        </View>
-                        
-                    </View>
-                }
             />
         ) 
+    }
+
+    const renderSubtitleSection = (text1, text2) => {
+        return(
+            <View style={{width: width*0.9, marginLeft: width*0.05 }}>
+                <View style={{ width: 0.9*width, flexDirection:'row', justifyContent: 'space-between' }}>
+                    <CustomText weight='bold' size={h2}>{text1}</CustomText>
+                    <TouchableOpacity
+                    >
+                        <CustomText size={h4} color={colors.primary}>Show All</CustomText>
+                    </TouchableOpacity>
+                    
+                </View>
+                
+                <CustomText size={h4} color={colors.grey_d}>{text2}</CustomText>
+            </View>
+        )
     }
 
     return (
@@ -234,7 +153,67 @@ export default function Main(){
             backgroundColor: colors.background
         }}>
 
-            {renderFlatlist(parties)}
+            <View style={{ width: width, marginVertical: 20}}>
+                <View style={{width: width*0.9, marginLeft: width*0.05 }}>
+                    <View style={{ width: 0.9*width, flexDirection:'row' }}>
+                        <CustomText weight='bold' size={h1}>Welcome</CustomText>
+                        <CustomText weight='bold' size={h1} color={colors.primary}> {currentUser.username}</CustomText>
+                    </View>
+                    <CustomText size={h4} color={colors.text}>Check out the map</CustomText>
+                        
+                    <MapView 
+                        style={{
+                            width: 0.9*width,
+                            height: 120,
+                            borderRadius: 10,
+                            marginTop: 10,
+                            marginBottom: 20,
+                        }}
+                        initialRegion={{
+                            latitude: 50.5107,
+                            longitude: 18.30056,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421,
+                        }}
+                        provider={PROVIDER_GOOGLE}
+                        onPress={() => navigation.navigate('Map', { location: currentLocation, isEvent})}
+                        zoomEnabled={false}
+                        rotateEnabled={false}
+                        scrollEnabled={false}
+                    />
+                </View>
+
+                {renderSubtitleSection('Popular Near You', 'Best parties')}
+
+                {parties ? 
+                
+                    <View style={{ marginTop: 10 }}>
+                        {renderHorizontal(parties, renderPopularItem)}
+                    </View>
+
+                    : 
+                    
+                    <View style={{width: '100%', height: 200, marginTop: 10, justifyContent: 'center', alignItems: 'center'}}>
+                        <ActivityIndicator />
+                    </View>
+                }
+                
+                
+                {renderSubtitleSection('Trending Users', 'Last week')}
+
+                {users ? 
+                
+                    <View style={{marginTop: 10}}>
+                        {renderHorizontal(users, renderUser)}
+                    </View>
+
+                    : 
+                    
+                    <View style={{width: '100%', height: 160, marginTop: 10, justifyContent: 'center', alignItems: 'center'}}>
+                        <ActivityIndicator />
+                    </View>
+                }
+            </View>
         </View>
     )
     
